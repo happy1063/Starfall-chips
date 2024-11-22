01e295d8f4ce993d04c26b8f1247854c4111f6e2
[MEF] мехроб <124378025+happy1063@users.noreply.github.com>
2024-11-22 08:12:15 +0300

    Update mute.lua
    added unmute command

--- mute.lua
+++ mute.lua
@@ -7,6 +7,10 @@ Blocks player messages (for yourself only)
 mute stupid_ming_2523562
 or
 mute STEAM_0:0:766803141
+
+unmute stupid_ming_2523562
+or
+unmute STEAM_0:0:766803141
 --]]
 if SERVER then
 local players
@@ -28,6 +32,21 @@ local finded
         end
 return ""
 end
+if ply == owner() and string.explode(" ",text)[1] == "unmute" then
+        players = find.allPlayers()
+        name = string.explode(" ",text)[2]
+        finded = 0
+        for i = 1 , table.count(players) do 
+            if players[i]:isValid() and (string.find(string.lower(players[i]:getName()),string.lower(name)) or players[i]:getSteamID() == name) then
+                player = players[i]
+                finded = 1
+        net.start("send2")
+        net.writeEntity(player)
+        net.send(ply)
+            end
+        end
+return ""
+end
         end)
 end
 
@@ -36,17 +55,14 @@ if CLIENT then
         net.readEntity(function(ent)
         hook.add("PlayerChat",ent:getName(),function(ply)
             if ply == ent then
-                return "1"
+                return ""
             end
         end)
         end)
 end)
+        net.receive("send2", function()
+        net.readEntity(function(ent)
+        hook.remove("PlayerChat",ent:getName())
+        end)
+end)
 end
-    
-    
-    
-    
-    
-    
-
-
